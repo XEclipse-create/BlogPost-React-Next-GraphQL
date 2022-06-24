18b0de788346a91eb1519c01343804da34ddfc23
XEclipse-create <60214398+XEclipse-create@users.noreply.github.com>
2022-06-24 22:57:15 +0530

    'refactor'

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss';
+
+import React, {useEffect, useState} from 'react'
+import {Layout}  from '../components'
+
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  )
+}
+
+export default MyApp
